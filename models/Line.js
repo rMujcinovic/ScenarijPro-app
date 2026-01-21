bcecafc1aee897411382c38b2206dd30ca0b8649
rMujcinovic <refikmm24@gmail.com>
2026-01-21 14:58:39 +0100

    feat(Line.js): definisi Line sequelize model

---
+++ models/Line.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../db");
+
+const Line = sequelize.define(
+  "Line",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+
+    lineId: { type: DataTypes.INTEGER, allowNull: false },
+
+    text: { type: DataTypes.TEXT, allowNull: false, defaultValue: "" },
+
+    nextLineId: { type: DataTypes.INTEGER, allowNull: true },
+  },
+  { tableName: "lines", timestamps: false }
+);
+
+module.exports = Line;
