d9d541bd8071da0d52f61a496c16cfb69e561df4
rMujcinovic <refikmm24@gmail.com>
2026-01-21 14:57:16 +0100

    kreiraj folder models i dodaj db.js file

---
+++ models/db.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("wt26", "root", "password", {
+  host: "127.0.0.1",
+  dialect: "mysql",
+  logging: false,
+});
+
+module.exports = { sequelize };
