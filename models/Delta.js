a78e6dbb24d3ef97efd7d9783cd6b1ef177b9f64
rMujcinovic <refikmm24@gmail.com>
2026-01-21 14:59:24 +0100

    feat(Delta.js): definisi Delta sequelize model

---
+++ models/Delta.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../db");
+
+const Delta = sequelize.define(
+  "Delta",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false, // "line_update" ili "char_rename"
+    },
+
+    lineId: { type: DataTypes.INTEGER, allowNull: true },
+    nextLineId: { type: DataTypes.INTEGER, allowNull: true },
+    content: { type: DataTypes.TEXT, allowNull: true },
+
+    oldName: { type: DataTypes.STRING, allowNull: true },
+    newName: { type: DataTypes.STRING, allowNull: true },
+
+    timestamp: { type: DataTypes.INTEGER, allowNull: false },
+  },
+  { tableName: "deltas", timestamps: false }
+);
+
+module.exports = Delta;
